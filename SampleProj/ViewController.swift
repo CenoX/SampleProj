f2f4f86a2ebd1aea96c147cc6937c9a932cf61ec
CenoX <CenoX@CenoXui-MacBook-Pro.local>
2016-03-23 13:19:14 +0900

    Initial Commit

---
+++ SampleProj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleProj
+//
+//  Created by CenoX on 2016. 3. 23..
+//  Copyright © 2016년 CenoX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
