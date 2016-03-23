941db8d9702e760bd6701f2ca7ef0a4e92928195
CenoX <CenoX@CenoXui-MacBook-Pro.local>
2016-03-23 13:28:19 +0900

    Initial Commit

--- SampleProj/ViewController.swift
+++ SampleProj/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import JLToast
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var menuItem: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.title = "Main"
+        self.navigationController?.navigationBar.tintColor = .whiteColor()
+        self.navigationController?.navigationBar.barTintColor = .blueColor()
+        self.navigationController?.navigationBar.translucent = false
+        self.navigationController?.navigationBar.opaque = true
+        self.navigationController?.navigationBar.barStyle = .Black
+        
+        // Add RevealViewController action
+        if self.revealViewController() != nil {
+            menuItem.target = self.revealViewController()
+            menuItem.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        
+        // PLEASE TEST WITH THIS LINE
+        JLToast.makeText("TEXT MAKING WITH JLTOAST").show()
     }
 
     override func didReceiveMemoryWarning() {
